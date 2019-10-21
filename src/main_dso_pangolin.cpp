b2c59552e916bd55e2bff06be77831cd848afd63
zeroAska <trotwoodray@gmail.com>
2019-10-21 13:45:46 -0400

    a small release

--- src/main_dso_pangolin.cpp
+++ src/main_dso_pangolin.cpp
@@ -65,7 +65,7 @@ bool reverse_dso = false;
 bool disableROS = false;
 
 int start=0;
-int end_dso=900;
+int end_dso=100000;
 
 bool prefetch = false;
 float playbackSpeed=0;	// 0 for linearize (play as fast as possible, while sequentializing tracking & mapping). otherwise, factor on timestamps.
